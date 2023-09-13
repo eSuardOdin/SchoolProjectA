72464e671de1ec1bab8e6b15289f5be933f7e6fc
eSuardOdin <esuardodin@gmail.com>
2023-09-13 08:53:34 +0200

    Scaffolded database

---
+++ TestEFCoreMySql/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestEFCoreMySql;
+
+public partial class BankAccount
+{
+    public int BankAccountId { get; set; }
+
+    public string BankAccountLabel { get; set; } = null!;
+
+    public decimal BankAccountBalance { get; set; }
+
+    public int MoniId { get; set; }
+
+    public virtual Moni Moni { get; set; } = null!;
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+}
