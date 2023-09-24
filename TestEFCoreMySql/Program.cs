7e8f4d8c0e192ec60aed5175d8ef7de650e2537a
eSuardOdin <esuardodin@gmail.com>
2023-09-24 14:29:05 +0200

    Tested some LINQ query

--- TestEFCoreMySql/Program.cs
+++ TestEFCoreMySql/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using TestEFCoreMySql;
+
+using (var context = new MoniWatchIContext())
+{
+    var accounts = context.BankAccounts.Include(a => a.Moni).ToList();
+    foreach(var account in accounts) Console.WriteLine($"Le compte {account.BankAccountLabel} appartient à {account.Moni.MoniLogin}");
+}
