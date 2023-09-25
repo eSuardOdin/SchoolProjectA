c1dba62f2b4a6dbfdc0c025a05607b332be22e32
eSuardOdin <esuardodin@gmail.com>
2023-09-25 20:09:25 +0200

    Decided to copy everything I already did

--- MoniWatchIApI/Controllers/MoniController.cs
+++ MoniWatchIApI/Controllers/MoniController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
-using TestEFCoreMySql;
 
 
 namespace MoniWatchIApI.Controllers;
@@ -17,7 +16,7 @@ public class MoniController : ControllerBase
     }
 
     // o----------------o
-    // | GET ALL MONIES |
+    // | GET ALL Monis |
     // o----------------o
     [HttpGet]
     [Route("GetAllMonis")]
@@ -27,6 +26,17 @@ public class MoniController : ControllerBase
     }
 
 
+
+    // o----------o
+    // | GET MONI |
+    // o----------o
+    /// <summary>
+    /// Returns a Moni with matching login and password</br>
+    /// Route URL: /root/moni/GetMoni?moniLogin={login}&moniPwd={pwd}
+    /// </summary>
+    /// <param name="moniLogin">The Login to search in db</param>
+    /// <param name="moniPwd">Password that will be tested against hashed password in db</param>
+    /// <returns>Not found or moni object</returns>
     [HttpGet]
     [Route("GetMoni")]
     public async Task<ActionResult<Moni>> GetMoni(string moniLogin, string moniPwd)
@@ -37,7 +47,7 @@ public class MoniController : ControllerBase
                 m => m.MoniLogin == moniLogin
             ).FirstOrDefaultAsync();
             
-            if (moni is null || /*!BcryptNet.Verify(moniPwd, moni.MoniPwd)*/ moniPwd != moni.MoniPwd)
+            if (moni is null || !BcryptNet.Verify(moniPwd, moni.MoniPwd) /*moniPwd != moni.MoniPwd*/)
             {
                 return NotFound();
             }
@@ -45,6 +55,100 @@ public class MoniController : ControllerBase
             {
                 return Ok(moni);
             }
+        }
+    }
+
+
+
+    // o-----------o
+    // | POST MONI | 
+    // o-----------o
+    /// <summary>
+    /// Adds a Moni to database with an hashed password</br>
+    /// With http POST request on URL: /root/moni/PostMoni
+    /// </summary>
+    /// <param name="moni">Moni object added, will be passed in request as JSON</param>
+    /// <returns>The Moni created</returns>
+    [HttpPost]
+    [Route("PostMoni")]
+    public async Task<ActionResult<Moni>> PostMoni([FromBody] Moni moni)
+    {
+        // Pwd encrypt
+        moni.MoniPwd = BcryptNet.HashPassword(moni.MoniPwd);
+        using (MoniWatchIContext db = new())
+        {
+            if(moni is null)
+            {
+                return BadRequest("Bad data provided");
+            }
+            db.Monis.Add(moni);
+            await db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMoni), new {moniLogin = moni.MoniLogin}, moni);
+
+        } 
+    }
+
+    // o-------------------o
+    // | UPDATE MONI LOGIN |
+    // o-------------------o
+    [HttpPatch]
+    [Route("UpdateMoniLogin")]
+    public async Task<ActionResult<Moni>> UpdateMoniLogin(int moniId, string moniLogin)
+    {
+        using (MoniWatchIContext db = new())
+        {
+            Moni moni = await db.Monis.FindAsync(moniId);
+            if (moni is null)
+            {
+                return BadRequest("Moni not found");
+            }
+            moni.MoniLogin = moniLogin;
+            await db.SaveChangesAsync();
+            return Ok($"Moni login successfully updated: {moni.MoniLogin}");
+        }
+    }
+
+
+    // o-----------------o
+    // | UPDATE MONI PWD |
+    // o-----------------o
+    [HttpPatch]
+    [Route("UpdateMoniPwd")]
+    public async Task<ActionResult<Moni>> UpdateMoniPwd(int moniId, string moniPwd)
+    {
+        using (MoniWatchIContext db = new())
+        {
+            Moni moni = await db.Monis.FindAsync(moniId);
+            if (moni is null)
+            {
+                return BadRequest("Moni not found");
+            }
+            // Pwd encrypt
+            moni.MoniPwd = BcryptNet.HashPassword(moniPwd);
+            await db.SaveChangesAsync();
+            return Ok($"Moni password successfully updated: {moni.MoniPwd}");
+        }
+    }
+
+
+    // o-------------o
+    // | DELETE MONI |
+    // o-------------o
+    [HttpDelete]
+    [Route("DeleteMoni")]
+    public async Task<ActionResult> DeleteMoni(int moniId)
+    {
+        using (MoniWatchIContext db = new())
+        {
+            Moni moni = await db.Monis.FindAsync(moniId);
+            if (moni is null)
+            {
+                return BadRequest("No Monis to delete");
+            }
+            db.Monis.Remove(moni);
+            await db.SaveChangesAsync();
+            return Ok($"Moni {moni.MoniLogin} has been deleted");
         }
     }
 
---
+++ MoniWatchIApI/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoniWatchIApI;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string TagLabel { get; set; } = null!;
+
+    public string? TagDescription { get; set; }
+
+    public int MoniId { get; set; }
+
+    public virtual Moni Moni { get; set; } = null!;
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+}
