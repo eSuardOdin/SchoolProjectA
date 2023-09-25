98f020990fb6635ab7eb686b2be44d6f95422937
eSuardOdin <esuardodin@gmail.com>
2023-09-25 20:22:17 +0200

    Deleted old projects

--- TestEFCoreMySql/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TestEFCoreMySql;
-
-using (var context = new MoniWatchIContext())
-{
-    var accounts = context.BankAccounts.Include(a => a.Moni).ToList();
-    foreach(var account in accounts) Console.WriteLine($"Le compte {account.BankAccountLabel} appartient à {account.Moni.MoniLogin}");
-}
